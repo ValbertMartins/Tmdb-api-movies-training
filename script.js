2cb91b0d2a2de13cfd90ce7c4bb23638f61669f3
valbert_martins <valbertclash@gmail.com>
2022-10-06 12:30:33 -0300

    dinamic search v1 added

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const topRatedMoviesCategoryEl = document.querySelector('.top-rated-movies-container')
+const urlBase = `https://api.themoviedb.org/3`
 const apiKey = '218aed1d10794deff7b827964c539e0c'
-const urlTopRatedMovies = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`
+const urlTopRatedMovies = `${urlBase}/movie/top_rated?api_key=${apiKey}`
 const urlImages = `https://image.tmdb.org/t/p/original`
-const urlDiscoverMovies = `https://api.themoviedb.org/3/discover/movie?`
+const urlDiscoverMovies = `${urlBase}/discover/movie?`
+const urlSearchMovies = `${urlBase}/search/movie?api_key=${apiKey}&query=`
 
+
+//request
 const request = async url => {
 
     try {
@@ -16,16 +20,27 @@ const request = async url => {
     }    
 }
 
-const createCategory = async (urlCategory,categoryEl) => {
-    const moviesList = await request(urlCategory)
 
-    moviesList.results.forEach( movie => {
-        const movieContainer = createMovieContainer()
-        categoryEl.appendChild(movieContainer)
-        movieContainer.append(createPosterMovie(movie),createTitleMovie(movie),createRankingImdb(movie))
-        
-        
-    });  
+
+//category-section
+const createCategory = async (urlCategory,categoryEl) => {
+    const { results:moviesList } = await request(urlCategory)
+    
+    if(moviesList.length == 0){
+        categoryEl.innerHTML = '<h1>Results not found</h1>'
+    } else{ 
+        categoryEl.innerHTML = ''
+        moviesList.forEach( movie => {
+            const movieContainer = createMovieContainer()
+            categoryEl.appendChild(movieContainer)
+            movieContainer.append(createPosterMovie(movie),createTitleMovie(movie),createRankingImdb(movie))
+                 
+        });  
+
+    }
+
+    
+
 }
 const createRankingImdb = movie => {
     const rankingImdb = document.createElement('span')
@@ -36,9 +51,11 @@ const createRankingImdb = movie => {
 
 const createPosterMovie = movie => {
     const img = document.createElement('img')
-    img.src = `https://image.tmdb.org/t/p/original${movie.poster_path}`
-    img.classList.add('poster-movie')
-    return img
+    if(movie.poster_path){
+        img.src = `https://image.tmdb.org/t/p/original${movie.poster_path}`
+        img.classList.add('poster-movie')
+        return img
+    } 
     
 } 
 
@@ -54,26 +71,15 @@ const createMovieContainer = () => {
     movieContainer.classList.add('movie-container')
     return movieContainer
 } 
-const init = () => {
-    createCategory(urlTopRatedMovies,topRatedMoviesCategoryEl)
-    
-}
-
-init()
-
-//request(urlTopRatedMovies)
-const body = document.querySelector('body')
-const title = document.querySelector('#title-outooor-movie')
 
 
-const createOutdoorMovie = async () => {    
+//outdoor-section
 
+const createOutdoorMovie = async () => {  
     const data = await request(`${urlDiscoverMovies}api_key=${apiKey}&page=${randomNumber(500)}&language=pt-BR`)
-    console.log(data)
     const { backdrop_path, title , vote_average , overview } = data.results[randomNumber(19)]
     outdoorDetails(title,backdrop_path,vote_average, overview)
-    body.style.opacity = 1
-    
+    document.querySelector('body').style.opacity = 1
       
 }
 
@@ -82,11 +88,56 @@ const outdoorDetails = (title,background,vote_average, overview) => {
     backgroundMovieEl.style.backgroundImage = `url(${urlImages}${background})`
     document.querySelector('#title-outooor-movie').innerText = title
     document.querySelector('.sinopsis-outdoor').innerText = overview
-
+    
 }
 
 const randomNumber = multiplier => Math.ceil(Math.random() * multiplier)
 
-createOutdoorMovie()
 
+
+//teste
+const hideOutdoor = (outdoorEL,resultSearchEl) => {
+    outdoorEL.style.display = 'none'
+    document.querySelector('.background-container-outdoor').style.backgroundSize = '0'
+    document.querySelector('.outdoor-gradient-container').style.minHeight = '0' 
+    resultSearchEl.style.display = 'flex'
+
+}
+const showOutdoor = (outdoorEL) => {
+    outdoorEL.style.display = 'block'
+    document.querySelector('.background-container-outdoor').style.backgroundSize = 'cover'
+    document.querySelector('.outdoor-gradient-container').style.minHeight = '100vh'
+}
+
+const hideCategory = category => category.style.display = 'none'
+
+const SearchListener =  () => {
+    const searchInput = document.querySelector('.search-input')
+    const resultSearchEl = document.querySelector('#results-category')
+    const outdoorEL = document.querySelector('.details-outdoor-container')
+
+    searchInput.addEventListener('keyup' , () => {
+        if(searchInput.value){
+            hideOutdoor(outdoorEL,resultSearchEl)
+            createCategory(urlSearchMovies + searchInput.value, resultSearchEl)
+           
+        } else {
+            showOutdoor(outdoorEL)
+            hideCategory(resultSearchEl)
+        }
+    })
+
+}
+
+
+const init = () => {
+    createOutdoorMovie()
+    createCategory(urlTopRatedMovies,topRatedMoviesCategoryEl)
+    SearchListener()
+    
+}
+
+
+
+init()
 
